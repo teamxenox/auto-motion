df21df5776613a27aeafc98d3ce47248291f2918
theapache64 <theapache64@gmail.com>
2020-02-25 11:58:39 +0530

    🆙 update install script

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 echo "⚙️ Installing..." &&
-pip install --user ffpb &&
-pip install autosub &&
-wget "https://github.com/theapache64/auto-motion/archive/master.zip" -q --show-progress &&
+pip3 install --user ffpb &&
+pip3 install autosub &&
+wget "https://github.com/theapache64/auto-motion/archive/master.zip" -q --show-progress -O "master.zip" &&
 unzip master.zip &&
 mv auto-motion-master auto-motion &&
 curDir=$PWD &&
 echo "\nalias auto-motion='java -jar $curDir/auto-motion/auto-motion_v1.0.0-alpha01.jar'" >> ~/.bash_aliases &&
 echo "👍 auto-motion installed!"
+
+
+
