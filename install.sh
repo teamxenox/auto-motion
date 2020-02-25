2086e7a9dc49f1a9be99b6bbafc6f7e1b9a88064
theapache64 <theapache64@gmail.com>
2020-02-25 11:52:07 +0530

    🆙 update install script

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
-wget "https://github.com/theapache64/auto-motion/archive/master.zip"
+echo "⚙️ Installing..." &&
+wget "https://github.com/theapache64/auto-motion/archive/master.zip" -q --show-progress &&
+unzip master.zip &&
+mv auto-motion-master auto-motion &&
+curDir=$PWD &&
+echo "\nalias auto-motion='java -jar $curDir/auto-motion/auto-motion_v1.0.0-alpha01.jar'" >> ~/.bash_aliases &&
+echo "👍 auto-motion installed!"
