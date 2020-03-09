597cc18aadb073047eacd837a3aa94649e278c1d
theapache64 <theapache64@gmail.com>
2020-03-09 11:41:09 +0530

    ⬆️ upgrade update script

--- update.sh
+++ update.sh
@@ -1,9 +1,12 @@
+LATEST_VERSION="v1.0.0-alpha01"
+
 echo "⚙️ Updating..." &&
-wget "https://github.com/theapache64/auto-motion/archive/master.zip" -q --show-progress -O "master.zip" &&
-unzip master.zip &&
-rm -R auto-motion &&
-mv auto-motion-master auto-motion &&
+wget "https://github.com/teamxenox/auto-motion/releases/download/$LATEST_VERSION/auto-motion-$LATEST_VERSION.zip"  -q --show-progress -O "auto-motion-$LATEST_VERSION.zip" &&
+sudo rm -R "auto-motion" &&
+unzip "auto-motion-$LATEST_VERSION.zip" -d "auto-motion" &&
+echo "➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️"
 echo "👍 auto-motion updated!"
+echo "➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️"
 
 
 
